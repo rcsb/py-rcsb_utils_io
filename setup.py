dac4161b93f0bdb3cdaeedaad0c67797adaf3052
jwest <john.westbrook@rcsb.org>
2019-07-06 10:23:57 -0400

    Move build artifacts to the end of build step

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url="https://github.com/rcsb/py-rcsb_utils_io",
     #
     license="Apache 2.0",
-    classifiers=(
+    classifiers=[
         "Development Status :: 3 - Alpha",
         # 'Development Status :: 5 - Production/Stable',
         "Intended Audience :: Developers",
@@ -38,7 +38,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
-    ),
+    ],
     entry_points={"console_scripts": []},
     #
     install_requires=["mmcif >= 0.40", "ruamel.yaml", "rcsb.utils.validation >= 0.14", "backports.tempfile"],
