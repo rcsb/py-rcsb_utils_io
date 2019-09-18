a773af02a158a9cd159c3a5fe8956d2452556dee
jwest <john.westbrook@rcsb.org>
2019-09-18 13:19:38 -0400

    Fix Py27 compatability issue with generator method

--- rcsb/utils/io/IoUtil.py
+++ rcsb/utils/io/IoUtil.py
@@ -545,7 +545,7 @@ class IoUtil(object):
         except Exception as e:
             logger.error("Unable to deserialize %r %s", filePath, str(e))
         #
-        return None
+        return
 
     def __deserializeCsv(self, filePath, delimiter=",", rowFormat="dict", encodingErrors="ignore", uncomment=True, **kwargs):
         oL = []
