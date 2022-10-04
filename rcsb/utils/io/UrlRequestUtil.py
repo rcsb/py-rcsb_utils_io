466aa72267ee556cafefc5a9b80ae93cb3710118
Dennis Piehl <piehld@gmail.com>
2022-10-03 21:12:52 -0400

    change param name

--- rcsb/utils/io/UrlRequestUtil.py
+++ rcsb/utils/io/UrlRequestUtil.py
@@ -218,7 +218,7 @@ class UrlRequestUtil(object):
 
         # encoding = kwargs.get("encoding", "utf-8")
         headerD = kwargs.get("headers", {})
-        addUserAgent = kwargs.get("addUserAgent", True)
+        overwriteUserAgent = kwargs.get("addUserAgent", True)
         exceptionsCatch = kwargs.get("exceptionsCatch", (HTTPError))
         httpCodesCatch = kwargs.get("httpCodesCatch", [])
         returnContentType = kwargs.get("returnContentType", None)
@@ -231,7 +231,7 @@ class UrlRequestUtil(object):
             if "Accept" not in headerD:
                 headerD["Accept"] = "application/json"
         #
-        if addUserAgent:
+        if overwriteUserAgent:
             headerD.update({"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"})
         #
         optD = {"timeout": timeOutSeconds, "allow_redirects": True, "verify": verify}
